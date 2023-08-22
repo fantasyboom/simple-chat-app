5b72a358a332688a472a4b88d1c3caa325bc3dec
unknown <m.borgaonker313@gmail.com>
2023-08-22 13:40:55 +0530

    ui changes eco-friendly chat

--- client.js
+++ client.js
@@ -1,10 +1,39 @@
 const socket = io();
 
-        // Handle form submission
-        const form = document.getElementById('chat-form');
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            var message = document.getElementById('message').value;
+// ... (existing code) ...
+
+let username = ''; // Variable to store the user's name
+
+// Handle joining the chat
+const joinButton = document.getElementById('join-button');
+const usernameInput = document.getElementById('username');
+const joinForm = document.querySelector('.join-form');
+const chatForm = document.getElementById('chat-form');
+const messagesList = document.getElementById('messages');
+const messageInput = document.getElementById('message');
+const sendButton = document.querySelector('.send-button');
+
+joinButton.addEventListener('click', () => {
+    username = usernameInput.value.trim();
+    if (username !== '') {
+        // Hide the join form
+        joinForm.style.display = 'none';
+        
+        // Show the chat form
+        chatForm.style.display = 'block';
+        messageInput.removeAttribute('disabled');
+        sendButton.removeAttribute('disabled');
+
+        // Emit a "user joined" message
+        socket.emit('user joined', username);
+    }
+});
+
+// Handle form submission
+const form = document.getElementById('chat-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var message = document.getElementById('message').value;
             var messages = document.getElementById('messages');
             const li = document.createElement('li');
             if(message[0]=="/"){
@@ -19,15 +48,19 @@ const socket = io();
                 document.getElementById('message').value = '';
             }
             else{
-            socket.emit('chat message', message);
-            document.getElementById('message').value = '';}
-        });
 
-        // Receive and display messages
-        socket.on('chat message', (msg) => {
-            const messages = document.getElementById('messages');
-            const li = document.createElement('li');
-            const mesg = msg.split(" ");
+    // Emit the message along with the username
+    socket.emit('chat message', { username, message });
+
+    // Clear the input field
+    document.getElementById('message').value = '';
+            }
+});
+
+// Receive and display messages
+socket.on('chat message', (msg) => {
+    const li = document.createElement('li');
+    const mesg = msg.message.split(" ");
             
             for(var i=0;i<mesg.length;i++){
                 const p=mesg[i].toLowerCase();
@@ -37,13 +70,13 @@ const socket = io();
                 case "lol" : mesg[i]="😂";break;
                 case "like" : mesg[i]="❤️";break;
                 case "congratulations" : mesg[i]="🎉";break;
+                case "india": mesg[i]="🇮🇳";break;
                 default: mesg[i]=mesg[i];break;
             }
         }
-        msg=mesg.join(" ");
-        
-        li.textContent = msg;
-         //console.log(li);
-        messages.appendChild(li);
-        
-        });
+        msg.message=mesg.join(" ");
+    li.innerHTML = `<strong>${msg.username}:</strong> ${msg.message}`;
+    messagesList.appendChild(li);
+});
+
+// ... (rest of your code) ...
